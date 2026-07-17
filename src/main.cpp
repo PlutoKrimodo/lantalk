ad1d98a2fe08a41e550c92193043a85784f74b76
pluto <pluto@LAPTOP-G6UNBO59.localdomain>
2026-07-17 18:09:52 +0800

    M1-D2 bind + listen on 0.0.0.0:8888

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,31 @@ int main(){
     if(listen_fd<0){
         fprintf(stderr,"socket() failed: %s\n",strerror(errno));
         return 1;
-    } 
-    printf("socket created, fd=%d\n",listen_fd);
+    }
+
+    int opt=1;
+    setsockopt(listen_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
+
+    sockaddr_in addr;
+    memset(&addr,0,sizeof(addr));
+    addr.sin_family=AF_INET;
+    addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    addr.sin_port=htons(8888);
+
+    if(bind(listen_fd,(sockaddr*)&addr,sizeof(addr))<0){
+        fprintf(stderr,"bind() failed:%s\n",strerror(errno));
+        close(listen_fd);
+        return 1;
+    }
+
+    if(listen(listen_fd,128)<0){
+        fprintf(stderr,"listen() failed:%s\n",strerror(errno));
+        close(listen_fd);
+        return 1;
+    }
+
+    printf("listening on 0.0.0.8888 (press Ctrl+C to exit)\n");
+    pause();
     close(listen_fd);
     return 0;
 }
