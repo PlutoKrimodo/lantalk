d5cd77d9c18c5e20b39226da4b69d0f65bd04c99
pluto <pluto@LAPTOP-G6UNBO59.localdomain>
2026-07-18 23:30:10 +0800

    M1-D4: loop echo + handle disconnect

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main(){
         fprintf(stderr,"socket() failed: %s\n",strerror(errno));
         return 1;
     }
-
+    //防止服务器重新连接时，有旧连接残留在TIME_WAIT状态，导致bind显示“Address already in use”   
     int opt=1;
     setsockopt(listen_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt));
 
@@ -35,24 +35,31 @@ int main(){
     }
 
     printf("listening on 0.0.0.0:8888 (press Ctrl+C to exit)\n");
+    while (true){
+        sockaddr_in client_addr;
+        socklen_t cli_len=sizeof(client_addr);
+        int conn_fd=accept(listen_fd,(sockaddr*)&client_addr,&cli_len);
+        if (conn_fd < 0) {
+            fprintf(stderr, "accept() failed: %s\n", strerror(errno));
+            continue;
+        }
+        printf("connection accepted: %s:%d\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+        while(true){
+            char buf[1024];
+            ssize_t n=read(conn_fd,buf,sizeof(buf));
+
+            if(n==0){
+                printf("client: %s%d closed connection\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+                break;
+            }else if(n<0){
+                fprintf(stderr,"read() failed: %s\n",strerror(errno));
+                break;
+            }
+            write(conn_fd,buf,n);
+        }
+        close(conn_fd);
+    }
     
-    sockaddr_in client_addr;
-    socklen_t cli_len=sizeof(client_addr);
-    int conn_fd=accept(listen_fd,(sockaddr*)&client_addr,&cli_len);
-    if (conn_fd < 0) {
-        fprintf(stderr, "accept() failed: %s\n", strerror(errno));
-        close(listen_fd);
-        return 1;
-    }
-    printf("connection accepted: %s:%d\n",inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
-
-    char buf[1024];
-    ssize_t n=read(conn_fd,buf,sizeof(buf));
-    if(n>0){
-        write(conn_fd,buf,n);
-    }
-    close(conn_fd);
-
     close(listen_fd);
     return 0;
 }
